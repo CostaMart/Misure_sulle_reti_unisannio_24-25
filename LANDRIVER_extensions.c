de406c7aefaf05ab24849877547c5fbff96b295e
CostaMart <126796996+CostaMart@users.noreply.github.com>
2024-05-31 16:21:53 +0200

    Update LANDRIVER_extensions.c
    aggiunta funzione per abilitare il WOL

--- LANDRIVER_extensions.c
+++ LANDRIVER_extensions.c
@@ -4,6 +4,40 @@
   *
   * */
 
+#define MII_LAN874X_PHY_RX_ADDRA ((uint16_t)0x8061U)
+#define MII_LAN874X_PHY_RX_ADDRB ((uint16_t)0x8062U)
+#define MII_LAN874X_PHY_RX_ADDRC ((uint16_t)0x8063U)
+#define MII_LAN874X_PHY_MCFGR			((uint16_t)0x8064U)
+#define MII_LAN874X_PHY_MMD_WOL_WUCSR		((uint16_t)0x8010U)
+
+void LAN8742_enable_wol(lan8742_Object_t *pObj){
+	 uint16_t val = 0x4200;
+
+     // passo 1 -- settiamo i bit 01 per abilitare PME sul led1
+	 // abilitiamo anche il self clear ponendo il bit 9 a 1
+	// 0x4202
+   // 0100001000000010
+	 LAN8742_write_mmd(pObj, MII_LAN874X_PHY_MMD_WOL_WUCSR, val);
+
+
+	 val = 0x1000;
+	// passo 1 impostiamo il tempo di reset per il self clear del bit PME
+	 LAN8742_write_mmd(pObj, MII_LAN874X_PHY_MCFGR, val);
+
+	 // imposto i registri ADDR per riconoscere il MAC nel magic packet
+	 // assumento che il MAC sia 00:80:E1:00:00:00
+
+	 // registro A:
+	 uint16_t A = 0x0000;
+	 LAN8742_write_mmd(pObj, MII_LAN874X_PHY_RX_ADDRA, A);
+	 // registro B:
+	 uint16_t B = 0x00E1;
+	 LAN8742_write_mmd(pObj, MII_LAN874X_PHY_RX_ADDRB, B);
+	 // registro C:
+	 uint16_t C = 0x8000;
+	 LAN8742_write_mmd(pObj, MII_LAN874X_PHY_RX_ADDRC, C);
+ }
+
 
 /**
   * @brief write in MMD register
